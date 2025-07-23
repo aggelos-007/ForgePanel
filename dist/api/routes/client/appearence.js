1172a9d0c8f1854fead4634d3e7c376ac5fd4cab
aggelos-007 <aggeloss007@gmail.com>
2025-07-23 18:36:20 +0300

    add change appearence

--- dist/api/routes/client/appearence.js
+++ dist/api/routes/client/appearence.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.data = void 0;
+const discord_js_1 = require("discord.js");
 const managers_1 = require("../../../managers");
 const apiserver_1 = require("../../structures/apiserver");
 exports.data = (0, apiserver_1.createRoute)({
@@ -10,6 +11,11 @@ exports.data = (0, apiserver_1.createRoute)({
         switch (c.req.method.toLowerCase()) {
             case "patch":
                 const json = await c.req.json();
+                const data = await new discord_js_1.REST().setToken(managers_1.Panel.config.bot.token).patch(discord_js_1.Routes.user("@me"), { body: json }).catch(() => null);
+                if (!data)
+                    return reply.msg(500, "Failed to change appearence");
+                managers_1.InstanceManager.sendMessage({ code: managers_1.ProcessCodes.ChangeAppearence });
+                return reply.succ();
             case "get":
                 const res = await managers_1.InstanceManager.askChild({ code: managers_1.ProcessCodes.Appearence });
                 if (res)
--- src/api/routes/client/appearence.ts
+++ src/api/routes/client/appearence.ts
@@ -1,4 +1,5 @@
-import { InstanceManager, ProcessCodes } from "../../../managers";
+import { AttachmentBuilder, REST, Routes } from "discord.js";
+import { InstanceManager, Panel, ProcessCodes } from "../../../managers";
 import { createRoute } from "../../structures/apiserver";
 
 interface IBody {
@@ -14,6 +15,10 @@ export const data = createRoute({
         switch(c.req.method.toLowerCase()){
             case "patch":
                 const json = await c.req.json() as IBody;
+                const data = await new REST().setToken(Panel.config.bot.token).patch(Routes.user("@me"), { body: json }).catch(() => null);
+                if(!data) return reply.msg(500, "Failed to change appearence");
+                InstanceManager.sendMessage({ code: ProcessCodes.ChangeAppearence })
+                return reply.succ()
             case "get":
                 const res = await InstanceManager.askChild({ code: ProcessCodes.Appearence })
                 if(res) return reply.succ(res.data);
